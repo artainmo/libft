053d08d1af644ef5ec7a12cd419317f02201370e
artainmo <53705599+artainmo@users.noreply.github.com>
2023-05-01 18:28:43 +0200

    Update ft_error.c

--- ft_error.c
+++ ft_error.c
@@ -17,3 +17,19 @@ void	ft_error(char *text)
 	write(2, text, ft_strlen(text));
 	exit(1);
 }
+
+void	ft_malloc_error(void)
+{
+	ft_putstr_fd("Malloc failed\n", 2);
+	exit(1);
+}
+
+void	ft_errno(void)
+{
+	char *error_message;
+
+	error_message = strerror(errno);
+	ft_putendl_fd(error_message, 2);
+	exit(1);
+
+}
